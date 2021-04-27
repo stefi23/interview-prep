ecd9a79eefbe5788714ca819bbdbd71ecb6920e3
stefi23 <stefania.rosca2309@gmail.com>
2021-04-27 14:48:22 +0200

    update to anagram added leetcode challenge

--- codewars/anagrams.js
+++ codewars/anagrams.js
@@ -1,4 +1,5 @@
 /*
+Where my anagrams at?
 https://www.codewars.com/kata/523a86aa4230ebb5420001e1
 
 Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
